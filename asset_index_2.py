5d68504ed84984ec2588cb5cf69453a4d9496108
rafflabs <123316155+rafflabs@users.noreply.github.com>
2023-01-27 13:19:31 +0100

    Create asset_index_2.py

---
+++ asset_index_2.py
@@ -0,0 +1,26 @@
+import datetime
+import yfinance as yf
+import plotly.express as px
+import plotly.graph_objects as go
+
+start = datetime.datetime(2009, 9, 26)
+end = datetime.datetime(2022, 9, 7)
+
+# MSCI_World_EUR = yf.download('SWDA.MI', start=start, end=end)
+VTI = yf.download('VTI', start=start, end=end)
+SPTL = yf.download('SPTL', start=start, end=end)
+IEF = yf.download('IEF', start=start, end=end)
+SPIP = yf.download('SPIP', start=start, end=end)
+DGL = yf.download('DGL', start=start, end=end)
+
+print("\nVTI\n", VTI)
+
+fig = go.Figure()
+# fig.add_trace(go.Scatter(x = MSCI_World_EUR.index, y = MSCI_World_EUR['Adj Close'], name = 'MSCI_World_EUR'))
+fig.add_trace(go.Scatter(x = VTI.index, y = VTI['Adj Close'], name = 'VTI'))
+fig.add_trace(go.Scatter(x = SPTL.index, y = SPTL['Adj Close'], name = 'SPTL'))
+fig.add_trace(go.Scatter(x = IEF.index, y = IEF['Adj Close'], name = 'IEF'))
+fig.add_trace(go.Scatter(x = SPIP.index, y = SPIP['Adj Close'], name = 'SPIP'))
+fig.add_trace(go.Scatter(x = DGL.index, y = DGL['Adj Close'], name = 'DGL'))
+fig.update_layout(title="ALL WEATHER PORTFOLIO")
+fig.show()
