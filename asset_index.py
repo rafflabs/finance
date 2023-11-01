69401a19cee4954755df25f55b103140310dd3e7
rafflabs <123316155+rafflabs@users.noreply.github.com>
2023-11-01 16:38:04 +0100

    Update asset_index.py

--- asset_index.py
+++ asset_index.py
@@ -33,8 +33,9 @@ print("\DGL\n", DGL)
 import plotly.graph_objects as go
 
 # GRAPH OF THE ASSETS IN THE "ALL WEATHER PORTFOLIO" by Ray Dalio
-# NOTE: this is for US investors, european investors should select al least Bonds in EUR currency
-# ===============================================================================================
+# NOTE: this portfolio is for US investors,
+#       european investors should select al least Bonds in EUR currency
+# ========================================================================
 
 fig = go.Figure()
 # fig.add_trace(go.Scatter(x = MSCI_World_EUR.index, y = MSCI_World_EUR['Adj Close'], name = 'MSCI_World_EUR'))
